d87bf819b9c97a844972c5e26ad630783b129334
aline <165079083+lineando@users.noreply.github.com>
2024-11-21 23:28:52 +0000

    a

---
+++ bd.js
@@ -0,0 +1,18 @@
+//bd.js
+import pkg from "pg";
+const { Pool } = pkg;
+
+//bd.js
+async function connecto() { // depois mudar o nome da função pra não confundir com o método connet do pool
+    const pool = new Pool({
+      connectionString: process.env.URL_BD,
+    });
+    return pool.connect();
+  }
+  //bd.js
+async function selectUsuarios() {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM usuario");
+    return res.rows;
+  }
+  export { selectUsuarios };
